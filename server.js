b4199e7be71af41ed93152c643adc414a912504f
vano <kolesen.ivan@gmail.com>
2019-10-13 13:48:45 +0300

    feat: different types of messages

--- server.js
+++ server.js
@@ -2,45 +2,29 @@ const express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
-
 const port = process.env.port || 8080;
-
-const server = app.listen(port, function(){
-  console.log('listening for requests');
-});
-
+const server = app.listen(port);
 const wss = new WebSocket.Server({ server });
 
 let userId = 1;
 let messageId = 1;
 
 wss.on('connection', ws => {
-  console.log("connected");
-
   ws.send(JSON.stringify({
     type: "auth",
-    value: {
-      id: userId++
-    }
+    userId: userId++
   }));
 
   ws.on('message', message => {
-    const recievedMessage = JSON.parse(message);
-    console.log(recievedMessage)
-    const sendingMessage = {
-      type: "message",
-      value: {
-        ...recievedMessage,
-        id: messageId++
-      }
-    };
+    const sendingMessage = JSON.stringify({
+      ...JSON.parse(message),
+      id: messageId++
+    });
 
     wss.clients.forEach( client => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(sendingMessage));
+        client.send(sendingMessage);
       }
     });
-
-    //ws.send(JSON.stringify(sendingMessage));
   })
 });
